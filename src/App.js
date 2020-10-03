a89b66ac31fd6c32d8c1ee7f95aeb13323f23f07
faizanansari1 <faizanshaikhansari@gmail.com>
2020-10-03 23:08:49 +0500

    add Material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from './NavBar';
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { Button, TextField, Theme } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+    },
+    paper: {
+      height: 400,
+      width: 500,
+      borderRadius:'20px',
+      backgroundColor: "#fff",
+      textAlign: "center",
+      margin: "60px auto",
+      padding:'20px',
+
+    },
+    form:{
+      display:'flex',
+      flexDirection: "column",
+     
+    },
+    input:{
+      marginBottom:'30px',
+    },
+  })
+);
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <NavBar />
+      <Paper elevation={4} className={classes.paper}>
+        <h1>Login</h1>
+
+        <form className={classes.form} noValidate autoComplete="off">
+          <TextField label="Username" variant="outlined" className={classes.input} />
+          <TextField label="Password" variant="outlined" className={classes.input}/>
+          <Button variant="contained" color="primary">
+            Login
+          </Button>
+        </form>
+      </Paper>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Expense Builder
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
